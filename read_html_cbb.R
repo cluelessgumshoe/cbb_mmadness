5b2cced23f46510a12ebc16af61cb6e591ece135
Erin Wright <erin.wright@stryker.com>
2020-03-11 18:44:32 -0400

    Update read_html_cbb.R
    most of cleaning is good, upped years to 10, added neat function to reorder columns found on stackO

--- read_html_cbb.R
+++ read_html_cbb.R
@@ -1,4 +1,10 @@
 
+#----------#
+# Analysis #
+#----------#
+#Time frame is last ten years (not including current year (2020))
+#Included only teams haveing been to tourey within time frame
+
 #-------#
 # Setup #
 #-------#
@@ -8,8 +14,50 @@ if (!require("pacman")) install.packages("pacman")
 library(pacman)
 
 # Load packages
-p_load(tidyverse, rvest, stringr, magrittr, lubridate)
+p_load(tidyverse, rvest, stringr, magrittr, lubridate,purrr)
 
+#----------------------#
+# Useful Functions     #
+#----------------------#
+#this one makes cleaning up team names below easier by removing teams not D1 in our range of years etc.
+`%not_in%` <- purrr::negate(`%in%`)
+
+#This function, credit to the link below helps reorder columns in the big frame after all the joins
+#the biggest benefit is that in this case where we have >40 columns, we can pick only the ones we want to
+#act on to name instad of having to name all like with this: data.table::setcolorder()
+##https://stackoverflow.com/questions/5620885/how-does-one-reorder-columns-in-a-data-frame 
+shuffle_columns <- function (invec, movecommand) {
+  movecommand <- lapply(strsplit(strsplit(movecommand, ";")[[1]],
+                                 ",|\\s+"), function(x) x[x != ""])
+  movelist <- lapply(movecommand, function(x) {
+    Where <- x[which(x %in% c("before", "after", "first",
+                              "last")):length(x)]
+    ToMove <- setdiff(x, Where)
+    list(ToMove, Where)
+  })
+  myVec <- invec
+  for (i in seq_along(movelist)) {
+    temp <- setdiff(myVec, movelist[[i]][[1]])
+    A <- movelist[[i]][[2]][1]
+    if (A %in% c("before", "after")) {
+      ba <- movelist[[i]][[2]][2]
+      if (A == "before") {
+        after <- match(ba, temp) - 1
+      }
+      else if (A == "after") {
+        after <- match(ba, temp)
+      }
+    }
+    else if (A == "first") {
+      after <- 0
+    }
+    else if (A == "last") {
+      after <- length(myVec)
+    }
+    myVec <- append(temp, values = movelist[[i]][[1]], after = after)
+  }
+  myVec
+}
 
 #----------------------#
 # Scrape data from web #
@@ -24,7 +72,7 @@ get_url_teams <- function(n_years){
   }
   return(url_teams)
 }
-url_teams <- get_url_teams(5)
+url_teams <- get_url_teams(11)
 
 get_cbb_teams <- function(url_teams){
   cbb_teams <- data_frame()
@@ -55,20 +103,20 @@ get_cbb_teams <- function(url_teams){
   return(cbb_teams)
 }
 cbb_teams <- get_cbb_teams(url_teams)
-
+feather::write_feather(cbb_teams,"cbb_teams.feather")
 
 #----------------#
 # combine & prep #
 #----------------#
 #use the NCAA text from the site to make a tourney field
-# cbb_df <- cbb_teams %>%
-#   arrange(school) %>%
-#   mutate(tourny = if_else(str_detect(school, "NCAA"),1,0),
-#          school = str_remove(school,"NCAA"),
-#          school = str_trim(school),
-#          row_num = row_number())
-# 
-# feather::write_feather(cbb_df,"data/cbb_df.feather")
+cbb_df <- cbb_teams %>%
+  arrange(school) %>%
+  mutate(tourny = if_else(str_detect(school, "NCAA"),1,0),
+         school = str_remove(school,"NCAA"),
+         school = str_trim(school),
+         row_num = row_number())
+
+feather::write_feather(cbb_df,"data/cbb_df.feather")
 cbb_df <- feather::read_feather("data/cbb_df.feather")
 #-----------------------------------------#
 # clean for use with dlm-1223 data rounds #
@@ -80,27 +128,37 @@ dlm_TourneySlots <- feather::read_feather("data/dlm-1223_data/TourneySlots.feath
 dlm_TourneyRounds <- feather::read_feather("data/dlm-1223_data/TourneyRounds.feather")
 
 dlm_Teams <- feather::read_feather("data/dlm-1223_data/Teams.feather")
-dlm_Teams %<>% filter(LastD1Season >= 2015) %>% mutate(Team_Full = str_to_lower(Team_Full)) %>% select(TeamID,Team_Full)
+dlm_Teams %<>% filter(LastD1Season >= 2011) %>% mutate(Team_Full = str_to_lower(Team_Full)) %>% select(TeamID,Team_Full)
 
 #make small frames to matchup team naming
-cbb_team <- cbb_df %>% select(row_num, school) %>% mutate(school = str_to_lower(school)) %>% distinct() %>% arrange(school)
-dlm_team <- dlm_Teams %>% select(Team_Full) %>% distinct() %>% arrange(Team_Full)
+cbb_team <- cbb_df %>% 
+  #select(row_num, school) %>% #used this while renaming schools with the anti-joins
+  select(-row_num) %>%
+  mutate(school = str_to_lower(school)) %>% 
+  distinct() %>% 
+  arrange(school)
+
+dlm_team <- dlm_Teams %>% 
+  #select(Team_Full) %>% #used this while renaming schools with the anti-joins
+  distinct() %>% 
+  arrange(Team_Full)
 
 
 #find the mismatches that need attention - there are 64
-#some will be removed because they have not been in NCAA and others renamed to match
-dlm_team_names <- anti_join(cbb_team,dlm_team, by = c("school" = "Team_Full"))
-`%not_in%` <- purrr::negate(`%in%`)
-cbb_team_c <- cbb_team %>% 
+#some will be removed because they have not been in NCAA and others renamed to match - once all matched up this should net 0 records on the anti-join
+#dlm_team_names <- anti_join(cbb_team,dlm_team, by = c("school" = "Team_Full"))
+#the not in function is used here
+cbb_team %<>% 
   filter(school %not_in% c("alabama-birmingham","cal state long beach",
                         "central florida","detroit mercy","florida international",
                         "fort wayne","green bay","purdue-fort wayne","south carolina upstate",
-                        "merrimack")) %>%
+                        "merrimack","winston-salem")) %>%
   mutate(school = str_replace(school, "-"," ")) %>%
   mutate(school = dplyr::recode(school,
                                 `albany (ny)` = "albany",
                                 #is = want; match cbb to teams
                                 "bowling green state" = "bowling green" ,
+                                "centenary (la)" = "centenary",
                                 "grambling" = "grambling state",
                                 "long island university" = "long island",
                                 "louisiana state" = "lsu",
@@ -116,7 +174,7 @@ cbb_team_c <- cbb_team %>%
                                 "milwaukee" = "wisconsin milwaukee",
                                 "middle tennessee" = "middle tennessee state",
                                 "nevada las vegas" = "nevada",
-                                "north carolina state" = "north carolina" ,
+                                "north carolina state" = "nc state" ,
                                 "north carolina greensboro" = "north carolina a&t",
                                 "north carolina greensboro" = "unc greensboro" ,
                                 "north carolina asheville" = "unc asheville",
@@ -140,29 +198,21 @@ cbb_team_c <- cbb_team %>%
                                 "university of california" = "california",
                                 "virginia commonwealth" = "virginia",
                                 "vmi" = "virginia military"
-                                )) 
-dlm_team_names <- anti_join(cbb_team_c,dlm_team, by = c("school" = "Team_Full")) %>% select(-row_num) %>% distinct()
+                                )) %>% distinct()
+#used the anti-join to make sure all schools were dealt with while aligning names - should yield zero rows
+#dlm_team_names <- anti_join(cbb_team,dlm_team, by = c("school" = "Team_Full"))  %>% distinct()
 
-#get ranks, seeds and positions
-cbb_team_c %<>% 
-  left_join(cbb_df,by = c("row_num"="row_num")) %>%
-  select(-school.y) %>%
-  rename(school = school.x) %>%
-  left_join(dlm_Teams,by = c("school" = "Team_Full")) %>%
-  left_join(dlm_TourneySeeds, by = c("TeamID"="TeamID","year" = "Season")) %>%
-  left_join(cbb, )
-  
 #get highest round data
 #https://www.kaggle.com/andrewsundberg/college-basketball-dataset/data#
-cbb <- read_csv("data/cbb.csv",)
+cbb <- read_csv("data/cbb.csv")
 cbb %<>% select(TEAM, CONF, POSTSEASON, SEED, YEAR) %>% 
-  mutate(row_num = row_number(),
+  mutate(
          TEAM = str_to_lower(TEAM),
          TEAM = str_replace(TEAM,"st\\.$","state"),
          TEAM = str_remove_all(TEAM,"\\'"),
          TEAM = str_remove_all(TEAM,"\\.")) %>%
   filter(TEAM %not_in% c("cal st fullerton","cal st northridge",
-                           "fort wayne","green bay","purdue-fort wayne")) %>%
+                           "fort wayne","green bay","purdue-fort wayne","winston-salem")) %>%
   mutate(TEAM = dplyr::recode(TEAM,
                                 #is = want; match cbb to teams
                               "arkansas little rock" = "arkansas",
@@ -177,7 +227,7 @@ cbb %<>% select(TEAM, CONF, POSTSEASON, SEED, YEAR) %>%
                               "milwaukee" = "wisconsin milwaukee",
                               "middle tennessee" = "middle tennessee state",
                               "nebraska omaha" = "nebraska",
-                              "north carolina state" = "north carolina" ,
+                              "north carolina state" = "nc state" ,
                               "penn" = "penn state",
                               "saint marys" = "st marys",
                               "saint peters" = "st peters",
@@ -189,27 +239,148 @@ cbb %<>% select(TEAM, CONF, POSTSEASON, SEED, YEAR) %>%
                               "ut rio grande valley" = "texas rgv",
                               "utsa" = "texas san antonio"
   )) 
-#does this match one of the other sets? ~of course not...
+#does this match one of the other sets? ~of course not...- once all matched up this should net 0 records on the anti-join
 #65 mismatch
 # cbb_names <- cbb %>%
 #   select(TEAM) %>%
 #   distinct() %>%
 #   anti_join(cbb_team, by = c("TEAM" = "school"))
 #27 mismatch
-cbb_names <- cbb %>%
-  select( TEAM) %>% #row_num,
-  distinct() %>%
-  anti_join(dlm_team, by = c("TEAM" = "Team_Full"))
+# cbb_names <- cbb %>%
+#   #select( TEAM) %>% #used this while renaming schools with the anti-joins
+#   distinct() %>%
+#   anti_join(dlm_team, by = c("TEAM" = "Team_Full"))
+
+#-------------------#
+# make a big frame  #
+#-------------------#
+#get ranks, seeds and positions
+cbb_team_c <- cbb_team %>% 
+  left_join(dlm_Teams,by = c("school" = "Team_Full")) %>%
+  left_join(dlm_TourneySeeds, by = c("TeamID"="TeamID","year" = "Season")) %>%
+  left_join(cbb, by = c("school" = "TEAM","year" = "YEAR"))  %>%
+  #select(-row_num.y,-row_num.x) %>%
+  rename(seed_num = SEED,
+         seed_bracket = Seed) %>%
+  arrange(school,year) %>%
+  mutate(result = case_when(
+    is.na(POSTSEASON) ~ 0,
+    POSTSEASON == "R68" ~ 1,
+    POSTSEASON == "R64" ~ 2,
+    POSTSEASON == "R32" ~ 3,
+    POSTSEASON == "S16" ~ 4,
+    POSTSEASON == "E8" ~ 5,
+    POSTSEASON == "F4" ~ 6,
+    POSTSEASON == "2nd" ~ 7,
+    POSTSEASON == "2ND" ~ 7,
+    POSTSEASON == "Champions" ~ 8,
+    TRUE ~ 0
+  ),
+  row_num = row_number()) %>%
+  filter(year != 2020) %>%
+  select(-CONF,-POSTSEASON,-seed_bracket,-seed_num) %>%
+  distinct()
+#just in case - will throw a helpful error if any are still same name and didn't join right after you run the next two lines
+names(cbb_team_c) %<>% stringr::str_remove_all(".x")
+names(cbb_team_c) %<>% stringr::str_remove_all(".y")
+names(cbb_team_c) %<>% stringr::str_to_lower()
+
+#Useful function to re-order (imported above)
+cbb_team_c %<>% .[shuffle_columns(names(cbb_team_c), "teamid first")] 
+cbb_team_c %<>% .[shuffle_columns(names(cbb_team_c), "school after teamid")] 
+cbb_team_c %<>% .[shuffle_columns(names(cbb_team_c), "rank after school")]
+#cbb_team_c %<>% .[shuffle_columns(names(cbb_team_c), "seed_num after rank")]
+#cbb_team_c %<>% .[shuffle_columns(names(cbb_team_c), "seed_bracket after seed_num")]
+cbb_team_c %<>% .[shuffle_columns(names(cbb_team_c), "result after rank")]
+cbb_team_c %<>% .[shuffle_columns(names(cbb_team_c), "year before school")] 
+
+cbb_team_c %<>%   mutate_at(.funs = list(as.numeric),.vars = vars(rank:tour)) 
+
+feather::write_feather(cbb_team_c,"data/cbb_team_c.feather")
+#-------------------#
+# exploratory       #
+#-------------------#
+#goal: predict the highest round 
+p_load(ggplot2, corrplot,DataExplorer)
+cbb_team_c %>% DataExplorer::plot_missing()
+cbb_team_c %>% select(-teamid,-year,-school) %>% as.matrix() %>% cor() %>% corrplot::corrplot()
 
 #-------------------#
 # prep for modeling #
 #-------------------#
 #goal: predict the highest round 
+p_load(caret,RANN)
+cbb_team_pdf <- cbb_team_c %>% 
+  #replace missing data with average for the given team
+  group_by(school) %>%
+  mutate(orb = case_when(is.na(orb) ~ mean(orb), TRUE ~ orb),
+         orb = case_when(is.na(orb) ~ mean(orb), TRUE ~ orb),
+         conf_l = case_when(is.na(conf_l) ~ mean(conf_l), TRUE ~ conf_l),
+         conf_w = case_when(is.na(conf_w) ~ mean(conf_w), TRUE ~ conf_w),
+         sos = case_when(is.na(sos) ~ mean(sos), TRUE ~ sos),
+         srs = case_when(is.na(srs) ~ mean(srs), TRUE ~ srs),
+         mp = case_when(is.na(mp) ~ mean(mp), TRUE ~ mp)) %>%
+  ungroup() %>% view()
+  select(-school,row_num, teamid) 
 
+#create training and test samples
+cbb_p_train <- createDataPartition(cbb_team_pdf$result ,p=0.7,list = F)
+cbb_train <- cbb_team_pdf[cbb_p_train,]
+preProcess_missingdata_model <- preProcess(cbb_train, method='knnImpute')
+cbb_train <- predict(preProcess_missingdata_model, newdata = cbb_train) 
+
+cbb_test <- cbb_team_pdf[-cbb_p_train,]
+
+#-----------#
+#  modeling #
+#-----------#
+
+ctrl <- trainControl(method = "repeatedcv", number = 10, repeats = 3,savePredictions = TRUE)
+
+#KNN Model
+cbb_knn <- caret::train(
+  result ~ .,
+  data = cbb_train,
+  method = "knn",
+  preProcess = c("center","scale"),
+  trControl = ctrl
+)
+saveRDS(cbb_knn,"cbb_knn.rds")
+
+cbb_knn
+pred_knn = predict(cbb_knn,cbb_test)
+pred_knn_accuracy = round(mean(pred_knn == cbb_test$result)*100,2)
+mean(mean(pred_knn != cbb_train$result))
+
+ggplot(data=cbb_knn$results, aes(x=k, y=RMSE)) +
+  geom_line() +
+  geom_point()
+
+
+#NN model
+cbb_nn <- caret::train(
+  result ~ .,
+  data = cbb_train,
+  method = "nnet",
+  preProcess = c("center","scale"),
+  trControl = ctrl,
+  trace=F
+)
+saveRDS(cbb_nn,"cbb_nn.rds")
+
+cbb_nn
+summary(cbb_nn)
+cbb_nn$results
+pred_nn = predict(cbb_nn,newdata=cbb_test)
+pred_nn_accuracy = round(mean(pred_nn == cbb_test$result)*100,4)
+
+p_load(NeuralNetTools)
+plotnet(cbb_nn, alpha = 0.5)
 
 #--------------#
 # housekeeping #
 #--------------#
 
-rm(list = c("url_ovrvw","url_teams","cbb","get_cbb_ovrvw","get_cbb_teams","get_url_ovrvw","get_url_teams"))
+rm(list = c("url_ovrvw","url_teams","cbb","get_cbb_ovrvw","get_cbb_teams","get_url_ovrvw","get_url_teams","dlm_team_names","cbb_names",
+            "dlm_TourneyRounds","dlm_TourneySeeds","dlm_TourneySlots","cbb_df","cbb_team","dlm_team","dlm_Teams"))
  
